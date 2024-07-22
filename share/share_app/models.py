8609eb13ef8e1c004756e8a95fa0f062819ec456
Kay Richter <kay.richter@googlemail.com>
2024-07-22 08:38:42 +0200

    Item (#3)
    * init
    
    * First test on item anon read
    
    * testing

--- share/share_app/models.py
+++ share/share_app/models.py
@@ -16,3 +16,12 @@ class Location(models.Model):
       unique_together = ('city', 'post_code',)
   def __str__(self):
     return json.dumps(self)
+
+class Item(models.Model):
+  description = models.CharField(max_length=164)
+  header = models.CharField(max_length=32)
+  location = models.ForeignKey(Location, on_delete=models.CASCADE)
+  category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return json.dumps(self)
---
+++ tests/test_item.py
@@ -0,0 +1,52 @@
+import pytest
+import random
+
+from django.urls import include, path, reverse
+from rest_framework.test import APITestCase, URLPatternsTestCase
+from rest_framework import status
+
+from share_app.models import Item, Category, Location
+from conftest import Clients
+
+class ItemTests(APITestCase, URLPatternsTestCase, Clients):
+    urlpatterns = [
+        path('', include('share.urls')),
+    ]
+
+    def setUp(self):
+        # Set up data for the whole TestCase
+        self.location = Location.objects.create(city="Hamburg", post_code="22525")
+        self.category =  Category.objects.create(name="Test2")
+        self.item = Item.objects.create(
+            description="An awesome thing to share",
+            header="This is short",
+            category = self.category,
+            location = self.location
+        )
+
+    def test_anon_read(self):
+        """
+        Ensure we can create a new account object.
+        """
+        url = reverse('items-list')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        url = reverse('items-detail', args=[1])
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["header"], "This is short")
+
+    def test_annon_no_write(self):
+        '''
+        Ensure that we can only announce new stuff if we dare known
+        '''
+        url = reverse('items-list')
+        data = {'city': 'Hamburg', 'post_code': 13371}
+        response = self.client.post(url,data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(len(response.data), 1)
+
+    def test_user_create(self):
+        # Include an appropriate `Authorization:` header on all requests.
+        pass
